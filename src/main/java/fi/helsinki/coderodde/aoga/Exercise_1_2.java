2177c2167c73d10d8ea6d32a1ee1cba17abb0907
coderodde <coderodd3@gmail.com>
2023-12-30 16:03:30 +0200

    Exercise 1.2 done.

---
+++ src/main/java/fi/helsinki/coderodde/aoga/Exercise_1_2.java
@@ -0,0 +1,163 @@
+package fi.helsinki.coderodde.aoga;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+class Exercise_1_2 {
+
+    public static void main(String[] args) {
+        long seed = System.currentTimeMillis();
+        Random random = new Random(seed);
+        System.out.println("seed = " + seed);
+        
+        Map<String, Integer> mapCodonToFrequencyMap = 
+                getFrequencyMapForEveryCodon(random);
+        
+        Map<Character, Map<String, Double>> mapCodonToProbability = 
+                getGlobalFrequencyMap(mapCodonToFrequencyMap);
+        
+        List<Character> alphabet = getAlphabet(mapCodonToProbability);
+        
+        String sourceProtein = Utils.generateRandomProteinSequence(alphabet, 
+                                                                   10, 
+                                                                   random);
+        
+        System.out.println("Source protein:  " + sourceProtein);
+        
+        String randomDNASequence =
+                generateRandomDNASequence(
+                        mapCodonToProbability, 
+                        sourceProtein, 
+                        random);
+        
+        System.out.println("Random sequence: " + randomDNASequence);
+    }
+    
+    private static List<Character> 
+        getAlphabet(Map<Character, Map<String, Double>> m) {
+        Set<Character> s = new HashSet<>();
+        
+        for (Map.Entry<Character, Map<String, Double>> e : m.entrySet()) {
+            s.add(e.getKey());
+        }
+        
+        return new ArrayList<>(s);
+    }
+    
+    static String generateRandomDNASequence(
+            Map<Character, Map<String, Double>> frequencyMap, 
+            String proteinSequence, 
+            Random random) {
+        
+        Utils.checkIsProtein(proteinSequence);
+        
+        StringBuilder dnaSequenceBuilder = new StringBuilder();
+        
+        for (char aminoAcid : proteinSequence.toCharArray()) {
+            String aminoAcidCodon = sampleRandomCodon(aminoAcid, 
+                                                      frequencyMap,
+                                                      random);
+            
+            dnaSequenceBuilder.append(aminoAcidCodon);
+        }
+        
+        return dnaSequenceBuilder.toString();
+    }
+    
+    private static String 
+        sampleRandomCodon(Character aminoAcid,
+                          Map<Character, Map<String, Double>> frequencyMap,
+                          Random random) {
+        
+        double coin = random.nextDouble();
+        Map<String, Double> m = frequencyMap.get(aminoAcid);
+        
+        for (Map.Entry<String, Double> e : m.entrySet()) {
+            String codon = e.getKey();
+            Double probability = e.getValue();
+            
+            if (coin <= probability) {
+                return codon;
+            }
+            
+            coin -= probability;
+        }
+        
+        throw new IllegalStateException("Should not get here.");
+    }
+    
+    private static Map<Character, Integer> 
+        getAminoAcidMap(Map<String, Integer> localMap) {
+        Map<Character, Integer> result = new HashMap<>();
+        
+        for (Map.Entry<String, Integer> e : localMap.entrySet()) {
+            String codon = e.getKey();
+            Integer count = e.getValue();
+            Character aminoAcid = Utils.getAminoAcid(codon);
+            Integer currentCount = result.getOrDefault(aminoAcid, 0) + count;
+            result.put(aminoAcid, currentCount);
+        }
+        
+        return result;
+    }
+    
+    static Map<Character, Map<String, Double>>
+         getGlobalFrequencyMap(Map<String, Integer> localMap) {
+             
+        Map<Character, Map<String, Double>> result = new HashMap<>();
+        Map<Character, Integer> aminoAcidMap = getAminoAcidMap(localMap);
+        
+        // Count the local frequencies:
+        for (Map.Entry<String, Integer> e : localMap.entrySet()) {
+            String codon = e.getKey();
+            Character aminoAcid = Utils.getAminoAcid(codon);
+            
+            if (!result.containsKey(aminoAcid)) {
+                result.put(aminoAcid, new HashMap<>());
+            }
+            
+            if (!result.get(aminoAcid).containsKey(codon)) {
+                result.get(aminoAcid).put(codon, 0.0);
+            }
+            
+            Double currentCodonFrequency = result.get(aminoAcid).get(codon);
+            result.get(aminoAcid).put(codon, 
+                                      currentCodonFrequency + 
+                                      localMap.get(codon));
+        }
+        
+        // Normalize the frequencies into probabilites:
+        for (Map.Entry<Character, Map<String, Double>> e : result.entrySet()) {
+            Character aminoAcid = e.getKey();
+            Integer aminoAcidCount = aminoAcidMap.get(aminoAcid);
+            
+            for (Map.Entry<String, Double> ee : e.getValue().entrySet()) {
+                Double value = ee.getValue();
+                Double probability = value / aminoAcidCount;
+                ee.setValue(probability);
+            }
+        }
+        
+        return result;
+    }
+         
+    private static Map<String, Integer> getFrequencyMapForEveryCodon(Random random) {
+        int samples = random.nextInt(200) + 1;
+        Map<String, Integer> frequencyMapCodonToFrequency = new HashMap<>();
+        
+        List<String> allCodons = Utils.CODONS;
+        
+        for (int i = 0; i < samples; i++) {
+            String codon = allCodons.get(random.nextInt(allCodons.size()));
+            Integer codonFrequency = 1 + random.nextInt(10);
+            frequencyMapCodonToFrequency.put(codon, codonFrequency);
+        }
+        
+        return frequencyMapCodonToFrequency;
+    }
+}
--- src/main/java/fi/helsinki/coderodde/aoga/Utils.java
+++ src/main/java/fi/helsinki/coderodde/aoga/Utils.java
@@ -2,8 +2,13 @@ package fi.helsinki.coderodde.aoga;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 import java.util.SortedMap;
 import java.util.SortedSet;
 import java.util.TreeMap;
@@ -12,35 +17,53 @@ import java.util.TreeSet;
 public class Utils {
     
     public static final SortedMap<Character, SortedSet<String>> 
-            MAP_AMINO_ACID_TO_CODON_SETS = new TreeMap<>();    
+            MAP_AMINO_ACID_TO_CODON_SETS;    
 
     public static final SortedMap<Character, List<String>> 
-            MAP_AMINO_ACID_TO_CODON_LISTS = new TreeMap<>();
+            MAP_AMINO_ACID_TO_CODON_LISTS;
+    
+    public static final List<Character> AMINO_ACID_ALPHABET_LIST;
+    public static final Set<Character> AMINO_ACID_ALPHABET_SET;
+    
+    public static final Map<String, Character> MAP_CODON_TO_AMINO_ACID;
+    
+    // The list of all codons:
+    public static final List<String> CODONS;
     
     static {
+        SortedMap<Character, List<String>> mapAminoAcidToCodonLists = 
+                new TreeMap<>();
         
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('A', Arrays.asList("GCT", "GCC", "GCA", "GCG"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('C', Arrays.asList("TGT", "TGC"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('D', Arrays.asList("GAT", "GAC"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('E', Arrays.asList("GAA", "GAG"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('F', Arrays.asList("TTT", "TTC"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('G', Arrays.asList("GGT", "GGC", "GGA", "GGG"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('H', Arrays.asList("CAT", "CAC"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('I', Arrays.asList("ATT", "ATC", "ATA"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('K', Arrays.asList("AAA", "AAG"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('L', Arrays.asList("TTA", "TTG", "CTT", "CTC", "CTA", "CTG"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('M', Arrays.asList("ATG"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('>', Arrays.asList("ATG"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('N', Arrays.asList("AAT", "AAC"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('P', Arrays.asList("CCT", "CCC", "CCA", "CCG"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('Q', Arrays.asList("CAA", "CAG"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('R', Arrays.asList("CGT", "CGC", "CGA", "CGG", "AGA", "AGG"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('S', Arrays.asList("TCT", "TCC", "TCA", "TCG", "AGT", "AGC"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('T', Arrays.asList("ACT", "ACC", "ACA", "ACG"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('V', Arrays.asList("GTT", "GTC", "GTA", "GTG"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('W', Arrays.asList("TGG"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('Y', Arrays.asList("TAT", "TAC"));
-        MAP_AMINO_ACID_TO_CODON_LISTS.put('<', Arrays.asList("TAA", "TGA", "TAG"));
+        mapAminoAcidToCodonLists.put('A', Arrays.asList("GCT", "GCC", "GCA", "GCG"));
+        mapAminoAcidToCodonLists.put('C', Arrays.asList("TGT", "TGC"));
+        mapAminoAcidToCodonLists.put('D', Arrays.asList("GAT", "GAC"));
+        mapAminoAcidToCodonLists.put('E', Arrays.asList("GAA", "GAG"));
+        mapAminoAcidToCodonLists.put('F', Arrays.asList("TTT", "TTC"));
+        mapAminoAcidToCodonLists.put('G', Arrays.asList("GGT", "GGC", "GGA", "GGG"));
+        mapAminoAcidToCodonLists.put('H', Arrays.asList("CAT", "CAC"));
+        mapAminoAcidToCodonLists.put('I', Arrays.asList("ATT", "ATC", "ATA"));
+        mapAminoAcidToCodonLists.put('K', Arrays.asList("AAA", "AAG"));
+        mapAminoAcidToCodonLists.put('L', Arrays.asList("TTA", "TTG", "CTT", "CTC", "CTA", "CTG"));
+        mapAminoAcidToCodonLists.put('M', Arrays.asList("ATG"));
+        mapAminoAcidToCodonLists.put('>', Arrays.asList("ATG"));
+        mapAminoAcidToCodonLists.put('N', Arrays.asList("AAT", "AAC"));
+        mapAminoAcidToCodonLists.put('P', Arrays.asList("CCT", "CCC", "CCA", "CCG"));
+        mapAminoAcidToCodonLists.put('Q', Arrays.asList("CAA", "CAG"));
+        mapAminoAcidToCodonLists.put('R', Arrays.asList("CGT", "CGC", "CGA", "CGG", "AGA", "AGG"));
+        mapAminoAcidToCodonLists.put('S', Arrays.asList("TCT", "TCC", "TCA", "TCG", "AGT", "AGC"));
+        mapAminoAcidToCodonLists.put('T', Arrays.asList("ACT", "ACC", "ACA", "ACG"));
+        mapAminoAcidToCodonLists.put('V', Arrays.asList("GTT", "GTC", "GTA", "GTG"));
+        mapAminoAcidToCodonLists.put('W', Arrays.asList("TGG"));
+        mapAminoAcidToCodonLists.put('Y', Arrays.asList("TAT", "TAC"));
+        mapAminoAcidToCodonLists.put('<', Arrays.asList("TAA", "TGA", "TAG"));
+        
+        MAP_AMINO_ACID_TO_CODON_LISTS =
+                Collections.unmodifiableSortedMap(mapAminoAcidToCodonLists);
+        
+        SortedMap<Character, SortedSet<String>> mapAminoAcidToCodonsSets = 
+                new TreeMap<>();
+        
+        MAP_AMINO_ACID_TO_CODON_SETS = Collections.unmodifiableSortedMap(mapAminoAcidToCodonsSets);
         
         // Order the codons in the alphabetic order:
         for (Map.Entry<Character, List<String>> e : MAP_AMINO_ACID_TO_CODON_LISTS.entrySet()) {
@@ -50,12 +73,64 @@ public class Utils {
         // Copy list codons to the (sorted) set codons:
         for (Map.Entry<Character, List<String>> e : MAP_AMINO_ACID_TO_CODON_LISTS.entrySet()) {   
             SortedSet<String> aminoAcidCodons = new TreeSet<>();
-            MAP_AMINO_ACID_TO_CODON_SETS.put(e.getKey(), aminoAcidCodons);
+            mapAminoAcidToCodonsSets.put(e.getKey(), aminoAcidCodons);
             
             for (String codon : e.getValue()) {
                 aminoAcidCodons.add(codon);
             }
         }
+        
+        Set<Character> aminoAcidAlphabet = new HashSet<>();
+        
+        aminoAcidAlphabet.add('A');
+        aminoAcidAlphabet.add('C');
+        aminoAcidAlphabet.add('D');
+        aminoAcidAlphabet.add('E');
+        aminoAcidAlphabet.add('F');
+        aminoAcidAlphabet.add('G');
+        aminoAcidAlphabet.add('H');
+        aminoAcidAlphabet.add('I');
+        aminoAcidAlphabet.add('K');
+        aminoAcidAlphabet.add('L');
+        aminoAcidAlphabet.add('M');
+        aminoAcidAlphabet.add('N');
+        aminoAcidAlphabet.add('P');
+        aminoAcidAlphabet.add('Q');
+        aminoAcidAlphabet.add('R');
+        aminoAcidAlphabet.add('S');
+        aminoAcidAlphabet.add('T');
+        aminoAcidAlphabet.add('V');
+        aminoAcidAlphabet.add('W');
+        aminoAcidAlphabet.add('Y');
+        
+        AMINO_ACID_ALPHABET_LIST = 
+                Collections.unmodifiableList(
+                        new ArrayList<>(aminoAcidAlphabet));
+        
+        AMINO_ACID_ALPHABET_SET = 
+                Collections.unmodifiableSet(aminoAcidAlphabet);
+        
+        Map<String, Character> mapCodonToAminoAcidCharacter = new HashMap<>();
+        
+        for (Map.Entry<Character, List<String>> e : MAP_AMINO_ACID_TO_CODON_LISTS.entrySet()) {
+            for (String codon : e.getValue()) {
+                mapCodonToAminoAcidCharacter.put(codon, e.getKey());
+            }
+        }
+        
+        MAP_CODON_TO_AMINO_ACID =
+                Collections.unmodifiableMap(mapCodonToAminoAcidCharacter);
+        
+        List<String> codons = new ArrayList<>(64);
+        
+        for (Map.Entry<Character, List<String>> e : MAP_AMINO_ACID_TO_CODON_LISTS.entrySet()) {
+            for (String codon : e.getValue()) {
+                codons.add(codon);
+            }
+        }
+        
+        codons.sort(Utils::compare);
+        CODONS = Collections.unmodifiableList(codons);
     }
     
     public static int compare(String str1, String str2) {
@@ -125,5 +200,21 @@ public class Utils {
     
     public static int getIntegerStringLength(int i) {
         return Integer.toString(i).length();
+    }
+    
+    public static Character getAminoAcid(String codon) {
+        return MAP_CODON_TO_AMINO_ACID.get(codon);
+    }
+    
+    public static String generateRandomProteinSequence(List<Character> alphabet,
+                                                       int length,
+                                                       Random random) {
+        StringBuilder sb = new StringBuilder(length);
+        
+        for (int i = 0; i < length; i++) {
+            sb.append(alphabet.get(random.nextInt(alphabet.size())));
+        }
+        
+        return sb.toString();
     }
 }
